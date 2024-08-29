fba5d6ef0195b6a55c51132ce0639c4e01637902
2547techno <technowizz2547@gmail.com>
2024-08-29 00:29:53 +0000

    clickable #channel text

--- src/messages/Link.hpp
+++ src/messages/Link.hpp
@@ -21,6 +21,7 @@ public:
         AutoModDeny,
         OpenAccountsPage,
         JumpToChannel,
+        JumpToOrCreateChannel,
         Reconnect,
         CopyToClipboard,
         ReplyToMessage,
